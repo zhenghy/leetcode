4c43d67cc51ead5a12d3e6c5b81eafc1d1e129f6
Haiyang Zheng <zhenghy@zls-iMac.local>
2019-12-22 09:01:40 +0800

    用c双向链表实现dict，并完成1.c

--- script/dict.c
+++ script/dict.c
@@ -1,38 +1,3 @@
-/*
- * @lc app=leetcode.cn id=1 lang=c
- *
- * [1] 两数之和
- *
- * https://leetcode-cn.com/problems/two-sum/description/
- *
- * algorithms
- * Easy (47.14%)
- * Likes:    7189
- * Dislikes: 0
- * Total Accepted:    731.3K
- * Total Submissions: 1.6M
- * Testcase Example:  '[2,7,11,15]\n9'
- *
- * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
- * 
- * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
- * 
- * 示例:
- * 
- * 给定 nums = [2, 7, 11, 15], target = 9
- * 
- * 因为 nums[0] + nums[1] = 2 + 7 = 9
- * 所以返回 [0, 1]
- * 
- * 
- */
-
-// @lc code=start
-
-/**
- * Note: The returned array must be malloced, assume caller calls free().
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -144,7 +109,7 @@ int view(NODE *dict)
 {
     NODE *p = dict;
     //开始
-    printf("====\nsize:%d", len(dict));
+    printf("====\nsize:%d",len(dict));
     printf("\n{");
     while (p)
     {
@@ -155,24 +120,14 @@ int view(NODE *dict)
     return 1;
 }
 
-int *twoSum(int *nums, int numsSize, int target, int *returnSize)
+int main()
 {
-    *returnSize = 2;
-    int *res = (int *)malloc(sizeof(int) * (*returnSize));
-    int *other = NULL;
-    NODE *dict;
-    for (int i = 0; i < numsSize; i++)
-    {
-        other= get(dict, target - nums[i]);
-        if (other == NULL)
-            set(&dict,nums[i],i); // 生成{num:index}的键值对
-        else
-        {
-            *res=*other;
-            *(res+1)=i;
-        }
-    }
-    return res;
+    NODE *dict = NULL;
+    set(&dict, 1, 1);
+    set(&dict, 2, 4);
+    set(&dict, 3, 27);
+    view(dict);
+    del(&dict, 3);
+    view(dict);
+    printf("%d\n", *get(dict, 2));
 }
-
-// @lc code=end
