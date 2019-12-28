a98211c1c1c12339bb0a807f98446f0369619faa
Haiyang Zheng <wnfdsfy@gmail.com>
2019-12-28 22:49:37 +0800

    doing 3.c

---
+++ leetcode_c/3.无重复字符的最长子串.c
@@ -0,0 +1,65 @@
+/*
+ * @lc app=leetcode.cn id=3 lang=c
+ *
+ * [3] 无重复字符的最长子串
+ *
+ * https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/description/
+ *
+ * algorithms
+ * Medium (32.19%)
+ * Likes:    2857
+ * Dislikes: 0
+ * Total Accepted:    295.2K
+ * Total Submissions: 916.9K
+ * Testcase Example:  '"abcabcbb"'
+ *
+ * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+ * 
+ * 示例 1:
+ * 
+ * 输入: "abcabcbb"
+ * 输出: 3 
+ * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: "bbbbb"
+ * 输出: 1
+ * 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+ * 
+ * 
+ * 示例 3:
+ * 
+ * 输入: "pwwkew"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+ * 请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+ * 
+ * 
+ */
+
+// @lc code=start
+
+
+int lengthOfLongestSubstring(char * s){
+    int map[128]={0};
+    int maxlen=0;
+    int i=0;
+    char tmp;
+    while(s[i]!='\0') {
+        tmp = (int)s[i];
+        if (map[tmp]==0){
+            map[tmp]=1;
+            maxlen ++;
+        }else{
+
+        }
+    }
+    return 3;
+
+}
+
+
+// @lc code=end
+
